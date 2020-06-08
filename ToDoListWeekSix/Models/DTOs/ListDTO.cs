7b5244f1c539b828f89ff37e1d6f5943f2ed0de9
cnickels21 <cnickels_21@yahoo.com>
2020-06-08 16:02:16 -0500

    list dto created

---
+++ ToDoListWeekSix/Models/DTOs/ListDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListWeekSix.Models.DTOs
+{
+    public class ListDTO
+    {
+    }
+}
