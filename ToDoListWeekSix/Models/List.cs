2d315c1b464f5817601b1eb787087384b40f522f
cnickels21 <cnickels_21@yahoo.com>
2020-06-08 15:10:47 -0500

    initial migration for to do list database

--- ToDoListWeekSix/Models/List.cs
+++ ToDoListWeekSix/Models/List.cs
@@ -7,6 +7,7 @@ namespace ToDoListWeekSix.Models
 {
     public class List
     {
+        public int Id { get; set; }
         public string Task { get; set; }
         public DateTime DueDate { get; set; }
         public string Assignee { get; set; }
