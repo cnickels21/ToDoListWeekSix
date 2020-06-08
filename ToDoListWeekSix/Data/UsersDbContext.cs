793a1e2e0bcfbba1be344bb28afe280b3c221096
cnickels21 <cnickels_21@yahoo.com>
2020-06-08 14:49:21 -0500

    users dbcontext

---
+++ ToDoListWeekSix/Data/UsersDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoListWeekSix.Models;
+
+namespace ToDoListWeekSix.Data
+{
+    public class UsersDbContext : IdentityDbContext<ToDoUser>
+    {
+        public UsersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
