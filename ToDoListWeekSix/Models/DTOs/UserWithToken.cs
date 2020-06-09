b157424155ad7579603df3b8ee4e68e25ce2ad48
cnickels21 <cnickels_21@yahoo.com>
2020-06-09 18:52:42 -0500

    todays lab in one big commit

---
+++ ToDoListWeekSix/Models/DTOs/UserWithToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListWeekSix.Models.DTOs
+{
+    public class UserWithToken
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
