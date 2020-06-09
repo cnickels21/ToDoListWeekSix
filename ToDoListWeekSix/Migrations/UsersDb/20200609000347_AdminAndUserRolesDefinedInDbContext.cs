4b84967099c7e1ff570c265845cc51e720eacd0c
cnickels21 <cnickels_21@yahoo.com>
2020-06-08 19:04:25 -0500

    migration for roles in user dbcontext

---
+++ ToDoListWeekSix/Migrations/UsersDb/20200609000347_AdminAndUserRolesDefinedInDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoListWeekSix.Migrations.UsersDb
+{
+    public partial class AdminAndUserRolesDefinedInDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "admin", "79575b8e-e8cc-414c-b257-082d1e0c8261", "administrator", null });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "user", "84ce0789-580f-4118-ade8-6488bfce2dd6", "user", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "user");
+        }
+    }
+}
